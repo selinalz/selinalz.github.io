bd167598ccf0819b0c5ae47d3d47106e77b70c9e
selinalz <71116616+selinalz@users.noreply.github.com>
2021-03-26 14:07:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,251 @@
+
+//box - Cross-Platform Storytelling
+//get "box" div using getElementsByClassName, put it inside a variable
+var link = document.getElementsByClassName("box")[0];
+//makes the variable listen for mouseover
+link.addEventListener("mouseover", Show);
+
+//when it mouse hovers over "box", trigger mouseover function that makes image appear and changes the color of the text
+function Show() {
+	var img = document.getElementsByClassName("image")[0];
+	img.style.opacity = "100%";
+	var txt = document.getElementsByClassName("link")[0];
+	txt.style.backgroundColor = "black";
+	txt.style.color = "white";
+	var circle = document.getElementsByClassName("circle")[0];
+	circle.style.backgroundColor = "black";
+}
+
+//get "box" div using getElementsByClassName, put it inside a variable
+var link = document.getElementsByClassName("box")[0];
+//makes the variable listen for mouseover
+link.addEventListener("mouseout", Hide);
+
+//when mouse moves away from the "box", trigger mouseout function that returns everything to default
+function Hide() {
+	var img = document.getElementsByClassName("image")[0];
+	img.style.opacity = "0%";
+	var txt = document.getElementsByClassName("link")[0];
+	txt.style.backgroundColor = "transparent";
+	txt.style.color = "black";
+	var circle = document.getElementsByClassName("circle")[0];
+	circle.style.backgroundColor = "transparent";
+}
+
+//same steps repeated for each of the "box" created for each assignment/exercise
+
+//box2 - Stories As Networks
+var link = document.getElementsByClassName("box2")[0];
+link.addEventListener("mouseover", Show2);
+
+function Show2() {
+	var img = document.getElementsByClassName("image2")[0];
+	img.style.opacity = "100%";
+	var txt = document.getElementsByClassName("link2")[0];
+	txt.style.backgroundColor = "black";
+	txt.style.color = "white";
+	var circle = document.getElementsByClassName("circle2")[0];
+	circle.style.backgroundColor = "black";
+}
+
+var link = document.getElementsByClassName("box2")[0];
+link.addEventListener("mouseout", Hide2);
+
+function Hide2() {
+	var img = document.getElementsByClassName("image2")[0];
+	img.style.opacity = "0%";
+	var txt = document.getElementsByClassName("link2")[0];
+	txt.style.backgroundColor = "transparent";
+	txt.style.color = "black";
+	var circle = document.getElementsByClassName("circle2")[0];
+	circle.style.backgroundColor = "transparent";
+}
+
+//box3 - Skills Test
+var link = document.getElementsByClassName("box3")[0];
+link.addEventListener("mouseover", Show3);
+
+function Show3() {
+	var img = document.getElementsByClassName("image3")[0];
+	img.style.opacity = "100%";
+	var txt = document.getElementsByClassName("link3")[0];
+	txt.style.backgroundColor = "black";
+	txt.style.color = "white";
+	var circle = document.getElementsByClassName("circle3")[0];
+	circle.style.backgroundColor = "black";
+}
+
+var link = document.getElementsByClassName("box3")[0];
+link.addEventListener("mouseout", Hide3);
+
+function Hide3() {
+	var img = document.getElementsByClassName("image3")[0];
+	img.style.opacity = "0%";
+	var txt = document.getElementsByClassName("link3")[0];
+	txt.style.backgroundColor = "transparent";
+	txt.style.color = "black";
+	var circle = document.getElementsByClassName("circle3")[0];
+	circle.style.backgroundColor = "transparent";
+}
+
+//box4 - Embedded Content
+var link = document.getElementsByClassName("box4")[0];
+link.addEventListener("mouseover", Show4);
+
+function Show4() {
+	var img = document.getElementsByClassName("image4")[0];
+	img.style.opacity = "100%";
+	var txt = document.getElementsByClassName("link4")[0];
+	txt.style.backgroundColor = "black";
+	txt.style.color = "white";
+	var circle = document.getElementsByClassName("circle4")[0];
+	circle.style.backgroundColor = "black";
+}
+
+var link = document.getElementsByClassName("box4")[0];
+link.addEventListener("mouseout", Hide4);
+
+function Hide4() {
+	var img = document.getElementsByClassName("image4")[0];
+	img.style.opacity = "0%";
+	var txt = document.getElementsByClassName("link4")[0];
+	txt.style.backgroundColor = "transparent";
+	txt.style.color = "black";
+	var circle = document.getElementsByClassName("circle4")[0];
+	circle.style.backgroundColor = "transparent";
+}
+
+//box5 - Translating Printed Media to Web Format
+var link = document.getElementsByClassName("box5")[0];
+link.addEventListener("mouseover", Show5);
+
+function Show5() {
+	var img = document.getElementsByClassName("image5")[0];
+	img.style.opacity = "100%";
+	var txt = document.getElementsByClassName("link5")[0];
+	txt.style.backgroundColor = "black";
+	txt.style.color = "white";
+	var circle = document.getElementsByClassName("circle5")[0];
+	circle.style.backgroundColor = "black";
+}
+
+var link = document.getElementsByClassName("box5")[0];
+link.addEventListener("mouseout", Hide5);
+
+function Hide5() {
+	var img = document.getElementsByClassName("image5")[0];
+	img.style.opacity = "0%";
+	var txt = document.getElementsByClassName("link5")[0];
+	txt.style.backgroundColor = "transparent";
+	txt.style.color = "black";
+	var circle = document.getElementsByClassName("circle5")[0];
+	circle.style.backgroundColor = "transparent";
+}
+
+//box6 - Multimedia Story
+var link = document.getElementsByClassName("box6")[0];
+link.addEventListener("mouseover", Show6);
+
+function Show6() {
+	var img = document.getElementsByClassName("image6")[0];
+	img.style.opacity = "100%";
+	var txt = document.getElementsByClassName("link6")[0];
+	txt.style.backgroundColor = "black";
+	txt.style.color = "white";
+	var circle = document.getElementsByClassName("circle6")[0];
+	circle.style.backgroundColor = "black";
+}
+
+var link = document.getElementsByClassName("box6")[0];
+link.addEventListener("mouseout", Hide6);
+
+function Hide6() {
+	var img = document.getElementsByClassName("image6")[0];
+	img.style.opacity = "0%";
+	var txt = document.getElementsByClassName("link6")[0];
+	txt.style.backgroundColor = "transparent";
+	txt.style.color = "black";
+	var circle = document.getElementsByClassName("circle6")[0];
+	circle.style.backgroundColor = "transparent";
+}
+
+//box7 - Media Queries + CSS Animation
+var link = document.getElementsByClassName("box7")[0];
+link.addEventListener("mouseover", Show7);
+
+function Show7() {
+	var img = document.getElementsByClassName("image7")[0];
+	img.style.opacity = "100%";
+	var txt = document.getElementsByClassName("link7")[0];
+	txt.style.backgroundColor = "black";
+	txt.style.color = "white";
+	var circle = document.getElementsByClassName("circle7")[0];
+	circle.style.backgroundColor = "black";
+}
+
+var link = document.getElementsByClassName("box7")[0];
+link.addEventListener("mouseout", Hide7);
+
+function Hide7() {
+	var img = document.getElementsByClassName("image7")[0];
+	img.style.opacity = "0%";
+	var txt = document.getElementsByClassName("link7")[0];
+	txt.style.backgroundColor = "transparent";
+	txt.style.color = "black";
+	var circle = document.getElementsByClassName("circle7")[0];
+	circle.style.backgroundColor = "transparent";
+}
+
+//box8 - Digital Zine
+var link = document.getElementsByClassName("box8")[0];
+link.addEventListener("mouseover", Show8);
+
+function Show8() {
+	var img = document.getElementsByClassName("image8")[0];
+	img.style.opacity = "100%";
+	var txt = document.getElementsByClassName("link8")[0];
+	txt.style.backgroundColor = "black";
+	txt.style.color = "white";
+	var circle = document.getElementsByClassName("circle8")[0];
+	circle.style.backgroundColor = "black";
+}
+
+var link = document.getElementsByClassName("box8")[0];
+link.addEventListener("mouseout", Hide8);
+
+function Hide8() {
+	var img = document.getElementsByClassName("image8")[0];
+	img.style.opacity = "0%";
+	var txt = document.getElementsByClassName("link8")[0];
+	txt.style.backgroundColor = "transparent";
+	txt.style.color = "black";
+	var circle = document.getElementsByClassName("circle8")[0];
+	circle.style.backgroundColor = "transparent";
+}
+
+var link = document.getElementsByClassName("box9")[0];
+link.addEventListener("mouseover", Show9);
+
+//box9 - Intro to Javascript
+function Show9() {
+	var img = document.getElementsByClassName("image9")[0];
+	img.style.opacity = "100%";
+	var txt = document.getElementsByClassName("link9")[0];
+	txt.style.backgroundColor = "black";
+	txt.style.color = "white";
+	var circle = document.getElementsByClassName("circle9")[0];
+	circle.style.backgroundColor = "black";
+}
+
+var link = document.getElementsByClassName("box9")[0];
+link.addEventListener("mouseout", Hide9);
+
+function Hide9() {
+	var img = document.getElementsByClassName("image9")[0];
+	img.style.opacity = "0%";
+	var txt = document.getElementsByClassName("link9")[0];
+	txt.style.backgroundColor = "transparent";
+	txt.style.color = "black";
+	var circle = document.getElementsByClassName("circle")[0];
+	circle.style.backgroundColor = "transparent";
+}
